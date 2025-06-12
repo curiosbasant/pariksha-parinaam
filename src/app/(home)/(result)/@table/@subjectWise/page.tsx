90889e509e486f63294a7cfb6578b3f0b1e8c53b
CuriosBasant <basantbrpl@gmail.com>
2025-06-12 19:21:12 +0530

    fix: 🐛 resolve calculation for subject wise summary

--- src/app/(home)/(result)/@table/@subjectWise/page.tsx
+++ src/app/(home)/(result)/@table/@subjectWise/page.tsx
@@ -21,23 +21,16 @@ const helpers = createColumnHelper<RowType>()
 const columns = [
   helpers.accessor('subjectName', { header: 'Subject Name' }),
   helpers.accessor('totalStudents', { header: 'Total Students' }),
+  helpers.accessor('passCount', { header: 'Pass' }),
   helpers.accessor('distinctionCount', { header: 'Distinction' }),
   helpers.accessor('firstDivisionCount', { header: 'First Division' }),
   helpers.accessor('secondDivisionCount', { header: 'Second Division' }),
   helpers.accessor('thirdDivisionCount', { header: 'Third Division' }),
-  helpers.accessor('passCount', { header: 'Pass' }),
   helpers.accessor('failCount', { header: 'Fail' }),
   helpers.display({
     header: 'Pass Percentage',
-    cell: ({ row }) => {
-      const passCount =
-        row.original.distinctionCount +
-        row.original.firstDivisionCount +
-        row.original.secondDivisionCount +
-        row.original.thirdDivisionCount +
-        row.original.passCount
-      return `${((passCount / row.original.totalStudents) * 100).toFixed(2)}%`
-    },
+    cell: ({ row }) =>
+      `${((row.original.passCount / row.original.totalStudents) * 100).toFixed(2)}%`,
   }),
 ]
 
@@ -48,21 +41,26 @@ export default function SubjectWiseTable() {
       arr[sub.name] ??= {
         subjectName: sub.name,
         totalStudents: 0,
+        passCount: 0,
         distinctionCount: 0,
         firstDivisionCount: 0,
         secondDivisionCount: 0,
         thirdDivisionCount: 0,
         failCount: 0,
-        passCount: 0,
       }
       arr[sub.name].totalStudents++
 
       if (sub.totalMarks < 33) arr[sub.name].failCount++
-      else if (sub.totalMarks < 40) arr[sub.name].passCount++
-      else if (sub.totalMarks < 50) arr[sub.name].thirdDivisionCount++
-      else if (sub.totalMarks < 60) arr[sub.name].secondDivisionCount++
-      else if (sub.totalMarks < 75) arr[sub.name].firstDivisionCount++
-      else arr[sub.name].distinctionCount++
+      else {
+        arr[sub.name].passCount++
+        if (sub.totalMarks < 40) {
+        } else if (sub.totalMarks < 50) arr[sub.name].thirdDivisionCount++
+        else if (sub.totalMarks < 60) arr[sub.name].secondDivisionCount++
+        else {
+          arr[sub.name].firstDivisionCount++
+          if (sub.totalMarks >= 75) arr[sub.name].distinctionCount++
+        }
+      }
     }
     return arr
   }, {} as Record<string, RowType>)
