2dce8fddd0b41d8d28e885d7cd328e414664ef7d
CuriosBasant <basantbrpl@gmail.com>
2025-06-15 14:38:45 +0530

    feat: ✨ add `ShareResultButton` to header

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -2,7 +2,10 @@
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Share2Icon } from 'lucide-react'
 import type { PropsWithChildren } from 'react'
+import { ClientOnly } from '~/components/client-only'
+import { Button } from '~/components/ui/button'
 import { getQueryClient } from '~/lib/query'
 
 export function Providers(props: PropsWithChildren) {
@@ -17,5 +20,28 @@ export function Providers(props: PropsWithChildren) {
       {props.children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+  )
+}
+
+export function ShareResultButton() {
+  return (
+    <ClientOnly>
+      {() => {
+        const shareData = { url: location.href }
+        if (!navigator.canShare?.(shareData)) return null
+        return (
+          <Button
+            variant='ghost'
+            size='icon'
+            onClick={async () => {
+              await navigator.share(shareData).catch(() => {})
+            }}
+            title='Share Result'
+            type='button'>
+            <Share2Icon />
+          </Button>
+        )
+      }}
+    </ClientOnly>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@ import './globals.css'
 import { Geist, Geist_Mono } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
-import { ComponentProps } from 'react'
+import type { ComponentProps } from 'react'
 import { ScrollArea } from '~/components/ui/scroll-area'
 import { getThemePreference } from './@themeSwitchButton/dal'
-import { Providers } from './client'
+import { Providers, ShareResultButton } from './client'
 import icon from './icon0.svg'
 
 const geistSans = Geist({
@@ -38,11 +38,20 @@ export default function RootLayout(props: LayoutProps<{ slots: 'themeSwitchButto
           <div className='isolate flex min-h-full w-full flex-col divide-y [--page-padding:--spacing(2)] [--page-size:var(--container-7xl)] sm:[--page-padding:--spacing(4)] md:[--page-padding:--spacing(8)]'>
             <header className='sticky top-0 z-10 bg-background/80 px-(--page-padding) backdrop-blur-sm'>
               <div className='m-auto flex max-w-(--page-size) items-center gap-4 py-3'>
-                <Link href='/' className='contents text-2xl font-extrabold'>
-                  <Image src={icon} className='not-dark:invert' width={32} height={32} alt='Logo' />
-                  Pariksha Parinaam
-                </Link>
-                <div className='ms-auto'>{props.themeSwitchButton}</div>
+                <div className='@container flex-1'>
+                  <Link href='/' className='inline-flex items-center gap-4'>
+                    <Image
+                      src={icon}
+                      className='not-dark:invert'
+                      width={26}
+                      height={26}
+                      alt='Logo'
+                    />
+                    <span className='text-xl font-extrabold @2xs:text-2xl'>Pariksha Parinaam</span>
+                  </Link>
+                </div>
+                <ShareResultButton />
+                {props.themeSwitchButton}
               </div>
             </header>
             <div className='flex-1 px-(--page-padding)'>
