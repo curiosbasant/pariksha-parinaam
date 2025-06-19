a4d52114d6b075632a59f99de25b64f637a1be2e
CuriosBasant <basantbrpl@gmail.com>
2025-06-19 16:12:25 +0530

    style: 💄 make table fade out on sides

--- src/app/(home)/(result)/@table/page.tsx
+++ src/app/(home)/(result)/@table/page.tsx
@@ -24,12 +24,12 @@ export default function ResultTableSlot() {
     ),
     helpers.accessor('marksObtained', { header: 'Marks Obtained' }),
     helpers.accessor('percentageText', { header: 'Percentage' }),
-    helpers.accessor('division', { header: 'Result' }),
+    helpers.accessor('division', { header: () => <div className='min-w-16'>Result</div> }),
   ]
 
   return (
     <div className='-mx-(--page-padding) flex'>
-      <div className='w-0 flex-1'>
+      <div className='w-0 flex-1 xl:mask-x-from-transparent xl:mask-x-to-black xl:mask-x-to-(--page-padding)'>
         <ScrollArea className='-mb-3 pb-3'>
           <div className='px-(--page-padding)'>
             <div className='overflow-clip rounded-md border'>
